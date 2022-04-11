29b9e99988d80eb5d4b5c2f680347b3102404ff3
Aravind Natesan <arvindh.aravind@gmail.com>
2022-04-10 22:45:33 -0400

    Updated both the addition and deletion tests

--- AddUser/adduser_spec.js
+++ AddUser/adduser_spec.js
@@ -1,4 +1,4 @@
-const { element, browser, Button } = require("protractor");
+const { element, browser, Button, $$, $ } = require("protractor");
 
 describe('Webtable Validation', function() {
 
@@ -7,9 +7,7 @@ describe('Webtable Validation', function() {
   })
 
   it('Test to add a user and to validate if the user has been added to the table', async function() {
-    browser.sleep(6000);
     await element(by.xpath("//button[contains(text(),'Add User')]")).click();
-    browser.sleep(10000);
     await  element(by.xpath("//input[@name='FirstName']")).sendKeys("Aravind");
     await element(by.xpath("//input[@name='LastName']")).sendKeys("Natesan");
     await element(by.xpath("//input[@name='UserName']")).sendKeys("AravindNatesan");
@@ -20,19 +18,43 @@ describe('Webtable Validation', function() {
     await element(by.xpath("//input[@name='Email']")).sendKeys("AravindNatesan@gmail.com");
     await element(by.xpath("//input[@name='Mobilephone']")).sendKeys("5555555555");
     await element(by.xpath("//button[contains(text(),'Save')]")).click();
-    browser.sleep(6000);
     expect(element.all(by.tagName('tr')).get(3).element(by.tagName('td')).getText()).toBe("Aravind");
+    let tableDetails=await $("table.smart-table tbody");
+    let rowDetails=tableDetails.$$("tr");
+    let noOfRows=rowDetails.count();
+    for(let i=0; i<noOfRows; i++)
+    {
+      let firstRowDet=await rowDetails.get(i).$$("td");
+      let firstData=await firstRowDet.get(0);
+      let text=await firstData.getText();
+      if(text === "Aravind")
+      {
+        console.log("The Record related to Aravind has been inserted successfully");
+        break;
+      }
+      else
+      console.log("The Record related to Aravind is not found")
+    }
   });
 
-    it('To delete Novac user and to validate if the user has been deleted from the table', async function() {
-      let rowCount = element.all(by.repeater('column in columns'));  
-      browser.sleep(6000);
-        rowCount.each(function(record){
-          let cells = record.$$('td');
-          cells.get(0).getText().then(function(txt){
-            if(txt == 'Novak')
-            cells.get(10).$(Button).click();
-          })
-        })
-    });
+  it('To delete Novac user and to validate if the user has been deleted from the table', async function() {
+    let tableDetails2=$("table.smart-table tbody");
+    let rowDetails2=tableDetails2.$$("tr");
+    let noOfRows2=await rowDetails2.count();
+    console.log(noOfRows2);
+    for(let i=0; i<noOfRows2; i++)
+    {
+      let firstRowDet2=rowDetails2.get(i).$$("td");
+      let firstData2=firstRowDet2.get(2);
+      let text2=await firstData2.getText();
+      if(text2 === "novak")
+      {
+        let deleteBtn=firstRowDet2.last().$(".icon.icon-remove");
+        await deleteBtn.click();
+        let okBtn=await element(by.xpath("//button[contains(text(),'OK')]"));
+        await okBtn.click();
+        break;
+      }
+    }
   });
+});
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Aravind_Natesan_teladoc_challenge
+# Run the test using the command protractor AddUser/conf.js
 Teladoc Automation Coding Challenge
+
+
